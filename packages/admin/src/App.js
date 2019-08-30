25a4956787be9521b0913fd02035cbdaac6e4f3a
Jack Scott <jack.rob.scott@gmail.com>
2019-08-30 11:57:23 +0800

    code so slick

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -4,21 +4,19 @@ import GraphiQL from 'graphiql'
 import 'graphiql/graphiql.css'
 
 export const App = () => {
-  const graphQLFetcher = graphQLParams => {
-    return fetch('http://localhost:4000', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(graphQLParams),
-    }).then(response => response.json())
-  }
   return component('div', {
     className: css({
       height: '100vh',
     }),
     children: component(GraphiQL, {
-      fetcher: graphQLFetcher,
+      fetcher: graphQLParams =>
+        fetch('http://localhost:4000', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(graphQLParams),
+        }).then(response => response.json()),
     }),
   })
 }
