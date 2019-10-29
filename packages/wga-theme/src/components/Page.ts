2d1bd215d8ada0ebdb75bac6a5c0c667e0bcb6d8
Jack Scott <jack.rob.scott@gmail.com>
2019-10-29 09:58:14 +0800

    added theme inside each component

--- packages/wga-theme/src/components/Page.ts
+++ packages/wga-theme/src/components/Page.ts
@@ -1,5 +1,7 @@
 import { createElement as create, FC } from 'react'
+import { useTheme } from '../contexts/Theme'
 
 export const Page: FC<{}> = ({}) => {
+  const theme = useTheme()
   return create('div')
 }
--- packages/wga-theme/src/components/SideBar.ts
+++ packages/wga-theme/src/components/SideBar.ts
@@ -1,5 +1,7 @@
 import { createElement as create, FC } from 'react'
+import { useTheme } from '../contexts/Theme'
 
 export const SideBar: FC<{}> = ({}) => {
+  const theme = useTheme()
   return create('div')
 }
