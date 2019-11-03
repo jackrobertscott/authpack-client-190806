2655546e6888fa00728c25a103de451a885be780
Jack Scott <jack.rob.scott@gmail.com>
2019-11-03 23:08:57 +0800

    merged plugin and client repos

--- packages/wga-plugin/src/index.ts
+++ packages/wga-plugin/src/index.ts
@@ -1,107 +1,13 @@
-import { Radio } from 'iframe-radio'
-import { settingsStore, ISettings } from './utils/settings'
-import { halter } from './utils/throttle'
+import { Schema } from './Schema'
 
-export type IPluginGadgets = ISettings['session']
+export type IAuthenticatorAPI = {
+  secret?: string
+  domain?: string
+  bearer?: string
+}
 
-export class PluginGadgets {
-  public update: () => void
-  private iframeId: string
-  private iframe?: HTMLIFrameElement
-  private radio?: Radio<{ name: string; payload?: any }>
-  private unlistener?: () => any
-  private ready: boolean
-  private queue: Array<() => void>
-  constructor(options: { suffix?: string; key: string }) {
-    this.update = halter(300, () => this.send('wga:update'))
-    this.iframeId = `wga-plugin${options.suffix ? `-${options.suffix}` : ''}`
-    this.render()
-    this.ready = false
-    this.queue = []
-    this.send('wga:domain', {
-      key: options.key,
-      url: document.location.origin,
-    })
-  }
-  /**
-   * Get the current state of the gadgets.
-   */
-  public get state(): IPluginGadgets {
-    return settingsStore.state.session
-  }
-  /**
-   * Listen to changes to the internal state.
-   */
-  public listen(callback: (current: ISettings['session']) => void) {
-    return settingsStore.listen(({ open = false, session }) => {
-      callback(session)
-      if (this.iframe) {
-        this.iframe.style.pointerEvents = open ? 'all' : 'none'
-      }
-    })
-  }
-  /**
-   * Open the gadgets.
-   */
-  public open() {
-    this.send('wga:open')
-  }
-  /**
-   * Open the gadgets.
-   */
-  private send(name: string, payload?: any) {
-    const message = () =>
-      this.radio &&
-      this.radio.message({
-        name,
-        payload,
-      })
-    if (this.ready) message()
-    else this.queue.push(message)
-  }
-  /**
-   * Create an iframe with gadgets.
-   */
-  private render() {
-    this.iframe = document.createElement('iframe')
-    this.iframe.src = document.location.hostname.includes('localhost')
-      ? 'http://localhost:3100/'
-      : 'https://plugin.wga.windowgadgets.io/'
-    this.iframe.id = this.iframeId
-    this.iframe.width = '100%'
-    this.iframe.height = '100%'
-    this.iframe.style.border = 'none'
-    this.iframe.style.boxShadow = 'none'
-    this.iframe.style.position = 'fixed'
-    this.iframe.style.top = '0'
-    this.iframe.style.bottom = '0'
-    this.iframe.style.right = '0'
-    this.iframe.style.left = '0'
-    this.iframe.style.zIndex = '1000'
-    this.iframe.style.transition = '200ms'
-    this.iframe.style.pointerEvents = 'none'
-    document.body.appendChild(this.iframe)
-    if (this.radio) this.radio.destroy()
-    this.radio = new Radio({
-      id: 'wga',
-      node: this.iframe.contentWindow,
-    })
-    if (this.unlistener) this.unlistener()
-    this.unlistener =
-      this.radio &&
-      this.radio.listen(({ name, payload }) => {
-        console.log(`Plugin received: ${name} - ${Date.now() % 86400000}`)
-        switch (name) {
-          case 'wga:set':
-            settingsStore.change(payload)
-            break
-          case 'wga:ready':
-            this.ready = true
-            this.queue.forEach(cb => cb())
-            break
-          default:
-            console.warn(`Handler not found for ${name}`)
-        }
-      })
+export class AuthenticatorAPI extends Schema {
+  constructor({ secret, domain, bearer }: IAuthenticatorAPI = {}) {
+    super({ keys: () => `${secret},${domain},${bearer}` })
   }
 }
