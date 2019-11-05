a4e7e94371358c54fb905542803fc217f31615f7
Jack Scott <jack.rob.scott@gmail.com>
2019-11-05 15:49:52 +0800

    added in all gadget routes

--- packages/gadgets/src/routers/RouterModalOnauthed.ts
+++ packages/gadgets/src/routers/RouterModalOnauthed.ts
@@ -5,6 +5,12 @@ import { UpdateUser } from '../screens/UpdateUser'
 import { ListProviders } from '../screens/ListProviders'
 import { RemoveUser } from '../screens/RemoveUser'
 import { UpdateUserPassword } from '../screens/UpdateUserPassword'
+import { CreateTeam } from '../screens/CreateTeam'
+import { SwitchTeam } from '../screens/SwitchTeam'
+import { UpdateTeam } from '../screens/UpdateTeam'
+import { CreateMembership } from '../screens/CreateMembership'
+import { ListMemberships } from '../screens/ListMemberships'
+import { RemoveTeam } from '../screens/RemoveTeam'
 
 export const RouterModalOnauthed: FC<{
   close: () => void
@@ -18,7 +24,18 @@ export const RouterModalOnauthed: FC<{
       { key: '/user/password', children: create(UpdateUserPassword) },
       { key: '/user/apps', children: create(ListProviders) },
       { key: '/user/danger', children: create(RemoveUser) },
-    ],
+      { key: '/team/create', children: create(CreateTeam) },
+      { key: '/team/switch', children: create(SwitchTeam) },
+    ].concat(
+      settings.state.session && settings.state.session.team
+        ? [
+            { key: '/team/update', children: create(UpdateTeam) },
+            { key: '/team/members/create', children: create(CreateMembership) },
+            { key: '/team/members', children: create(ListMemberships) },
+            { key: '/team/danger', children: create(RemoveTeam) },
+          ]
+        : []
+    ),
   })
   if (!settings.state.session) return null
   return create(Layout, {
@@ -59,7 +76,49 @@ export const RouterModalOnauthed: FC<{
           {
             icon: 'users',
             label: 'Team',
-            options: settings.state.session.team ? [] : [],
+            options: [
+              {
+                icon: 'plus',
+                label: 'Create New Workspace',
+                helper: 'Make a new workspace',
+                click: () => router.change('/team/create'),
+              },
+              {
+                icon: 'sync-alt',
+                label: 'Switch Workspace',
+                helper: 'Change to another workspace',
+                click: () => router.change('/team/switch'),
+              },
+            ].concat(
+              settings.state.session.team
+                ? [
+                    {
+                      icon: 'cog',
+                      label: 'Update Workspace',
+                      helper: 'Your workspace settings',
+                      click: () => router.change('/team/update'),
+                    },
+                    {
+                      icon: 'user-plus',
+                      label: 'Add Member',
+                      helper: 'Add a new workspace member',
+                      click: () => router.change('/team/members/create'),
+                    },
+                    {
+                      icon: 'user-plus',
+                      label: 'See Members',
+                      helper: 'List all workspace members',
+                      click: () => router.change('/team/members'),
+                    },
+                    {
+                      icon: 'fire-alt',
+                      label: 'Danger Zone',
+                      helper: 'Remove this workspace',
+                      click: () => router.change('/team/danger'),
+                    },
+                  ]
+                : []
+            ),
           },
           {
             icon: 'times-circle',
