fe489e2eadb50e46e7bacdd82ed49e77ccf621d1
Jack Scott <jack.rob.scott@gmail.com>
2019-12-31 17:11:46 +0800

    updated the plugin scripts

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,17 +1,24 @@
-import { Authpack } from '@authpack/sdk'
+import { Authpack, IPlugin } from '@authpack/sdk'
 
 if (!window) {
   const message = `Please ensure Authpack scripts are used in a browser`
   throw new Error(message)
+}
+interface IContext {
+  authpack: Authpack
+  state: IPlugin
 }
 /**
  * Run initial events once the page has loaded.
  */
 window.addEventListener('load', () => {
   const authpack = load()
-  guard(authpack)
-  open(authpack)
-  replace(authpack)
+  const dispatch = (context: IContext) =>
+    [guard, plugin, replace, show, hide].map(dispatcher => {
+      return dispatcher(context)
+    })
+  dispatch({ authpack, state: authpack.plugin.current() })
+  authpack.plugin.listen(state => dispatch({ authpack, state }))
 })
 /**
  * Locate the authpack script tag and initialise the authpack instance.
@@ -36,7 +43,7 @@ const load = () => {
 /**
  * Redirect unauthenticated users from protected pages.
  */
-const guard = (authpack: Authpack) => {
+const guard = (context: IContext) => {
   const node: HTMLElement = document.querySelector(`[data-authpack="guard"]`)
   if (!node) {
     return
@@ -46,46 +53,97 @@ const guard = (authpack: Authpack) => {
     console.warn(message)
     return
   }
-  authpack.plugin.listen(state => {
-    if (state.ready && !state.user) {
-      window.location.assign(node.dataset.redirect)
-    }
-  })
+  if (context.state.ready && !context.state.user) {
+    window.location.assign(node.dataset.redirect)
+  }
 }
 /**
  * Open the gadgets when item is clicked.
  */
-const open = (authpack: Authpack) => {
-  const node: HTMLElement = document.querySelector(`[data-authpack="show"]`)
+const plugin = (context: IContext) => {
+  const node: HTMLElement = document.querySelector(`[data-authpack="plugin"]`)
   if (!node) {
     return
   }
-  node.addEventListener('click', event => {
-    event.preventDefault()
-    authpack.plugin.show()
-  })
+  if (node.dataset.listening !== 'true') {
+    node.dataset.listening = 'true'
+    node.addEventListener('click', event => {
+      context.authpack.plugin.show()
+      event.preventDefault()
+    })
+  }
 }
 /**
  * Replace attributes with currect state.
  */
-const replace = (authpack: Authpack) => {
+const replace = (context: IContext) => {
   const data = document.querySelectorAll(`[data-authpack="replace"]`)
   const nodes = [...data] as HTMLElement[]
   if (!nodes.length) {
     return
   }
-  authpack.plugin.listen(state => {
-    if (state.ready && state.user) {
-      nodes.forEach(node => {
-        if (!node.dataset.value) {
-          const message = `Authpack replace tag missing "data-value" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-value="user.name"\n></div>`
-          console.warn(message)
-          return
-        }
-        const steps = node.dataset.value.split('.').map(i => i.trim())
-        const value = steps.reduce((accum, next) => accum[next], state)
-        node.textContent = value
-      })
-    }
-  })
+  if (context.state.ready && context.state.user) {
+    nodes.forEach(node => {
+      if (!node.dataset.value) {
+        const message = `Authpack replace tag missing "data-value" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-value="user.name"\n></div>`
+        console.warn(message)
+        return
+      }
+      const steps = node.dataset.value.split('.').map(i => i.trim())
+      const value = steps.reduce((accum, next) => accum[next], context.state)
+      node.textContent = value
+    })
+  }
+}
+/**
+ * Show an element when it satisfies any of the conditions.
+ */
+const show = (context: IContext) => {
+  const data = document.querySelectorAll(`[data-authpack="show"]`)
+  const nodes = [...data] as HTMLElement[]
+  if (!nodes.length) {
+    return
+  }
+  if (context.state.ready && context.state.user) {
+    nodes.forEach(node => {
+      const assertions = [
+        !node.dataset.plan ||
+          node.dataset.plan
+            .split(',')
+            .map(plan => plan.trim())
+            .includes(context.state.plan.tag),
+        // todo: more conditions
+      ]
+      node.dataset.display = node.dataset.display || node.style.display
+      node.style.display = assertions.includes(false)
+        ? 'none'
+        : node.dataset.display || node.style.display
+    })
+  }
+}
+/**
+ * Hide an element when it satisfies any of the conditions.
+ */
+const hide = (context: IContext) => {
+  const data = document.querySelectorAll(`[data-authpack="hide"]`)
+  const nodes = [...data] as HTMLElement[]
+  if (!nodes.length) {
+    return
+  }
+  if (context.state.ready && context.state.user) {
+    nodes.forEach(node => {
+      const assertions = [
+        !node.dataset.plan ||
+          node.dataset.plan
+            .split(',')
+            .map(plan => plan.trim())
+            .includes(context.state.plan.tag),
+        // todo: more conditions
+      ]
+      node.dataset.display = node.dataset.display || node.style.display
+      node.style.display = assertions.includes(true)
+        ? 'none'
+        : node.dataset.display || node.style.display
+    })
+  }
 }
