262b250695d3f680c5d65f50278633cbe27f7b5e
Jack Scott <jack.rob.scott@gmail.com>
2020-01-06 18:36:47 +0800

    ensure app is loaded before rendering

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -66,36 +66,73 @@ const load = () => {
  * Open the gadgets when item is clicked.
  */
 const plugin = (context: IContext) => {
+  if (!context.state.ready) {
+    return
+  }
   const data = document.querySelectorAll(`[data-authpack="open"]`)
   const nodes = [...data] as HTMLElement[]
   if (!nodes.length) {
     return
   }
-  if (context.state.ready) {
-    nodes.forEach(node => {
-      if (node.dataset.listening !== 'true') {
-        node.dataset.listening = 'true'
-        node.addEventListener('click', event => {
-          context.authpack.plugin.show()
-          event.preventDefault()
-        })
-      }
-    })
-  }
+  nodes.forEach(node => {
+    if (node.dataset.listening !== 'true') {
+      node.dataset.listening = 'true'
+      node.addEventListener('click', event => {
+        context.authpack.plugin.show()
+        event.preventDefault()
+      })
+    }
+  })
 }
 /**
  * Replace attributes with currect state.
  */
 const replace = (context: IContext) => {
+  if (!context.state.ready) {
+    return
+  }
   const data = document.querySelectorAll(`[data-authpack="replace"]`)
   const nodes = [...data] as HTMLElement[]
   if (!nodes.length) {
     return
   }
-  if (context.state.ready) {
-    nodes.forEach(node => {
-      if (!node.dataset.property) {
-        const message = `Authpack replace tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-property="user.name"\n></div>`
+  nodes.forEach(node => {
+    if (!node.dataset.property) {
+      const message = `Authpack replace tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-property="user.name"\n></div>`
+      console.warn(message)
+      return
+    }
+    const steps = node.dataset.property.split('.').map(i => i.trim())
+    const value = steps.reduce(
+      (accum, next) => accum && accum[next],
+      context.state
+    )
+    node.textContent =
+      typeof value === 'object' ? JSON.stringify(value, null, 2) : value
+  })
+}
+/**
+ * Redirect unauthenticated users from protected pages.
+ */
+const guard = (context: IContext) => {
+  if (!context.state.ready) {
+    return
+  }
+  const data = document.querySelectorAll(`[data-authpack="guard"]`)
+  const nodes = [...data] as HTMLElement[]
+  if (!nodes.length) {
+    setTimeout(() => postready(context))
+    return
+  }
+  const assertions = nodes.map(node => {
+    if (!node.dataset.redirect) {
+      const message = `Authpack guard tag missing "data-redirect" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n></div>`
+      console.warn(message)
+      return
+    }
+    if (node.dataset.property) {
+      if (!node.dataset.validate) {
+        const message = `Authpack guard tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
         console.warn(message)
         return
       }
@@ -104,60 +141,26 @@ const replace = (context: IContext) => {
         (accum, next) => accum && accum[next],
         context.state
       )
-      node.textContent =
-        typeof value === 'object' ? JSON.stringify(value, null, 2) : value
-    })
-  }
-}
-/**
- * Redirect unauthenticated users from protected pages.
- */
-const guard = (context: IContext) => {
-  const data = document.querySelectorAll(`[data-authpack="guard"]`)
-  const nodes = [...data] as HTMLElement[]
-  if (!nodes.length) {
-    setTimeout(() => postready(context))
-    return
-  }
-  if (context.state.ready) {
-    const assertions = nodes.map(node => {
-      if (!node.dataset.redirect) {
-        const message = `Authpack guard tag missing "data-redirect" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n></div>`
-        console.warn(message)
-        return
-      }
-      if (node.dataset.property) {
-        if (!node.dataset.validate) {
-          const message = `Authpack guard tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+      switch (node.dataset.validate) {
+        case 'present':
+          if (!!value) return
+          break
+        case 'missing':
+          if (!value) return
+          break
+        default:
+          const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
           console.warn(message)
           return
-        }
-        const steps = node.dataset.property.split('.').map(i => i.trim())
-        const value = steps.reduce(
-          (accum, next) => accum && accum[next],
-          context.state
-        )
-        switch (node.dataset.validate) {
-          case 'present':
-            if (!!value) return
-            break
-          case 'missing':
-            if (!value) return
-            break
-          default:
-            const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
-            console.warn(message)
-            return
-        }
-      } else if (context.state.user) {
-        return
       }
-      window.location.assign(node.dataset.redirect)
-      return true
-    })
-    if (!assertions.includes(true)) {
-      setTimeout(() => postready(context))
+    } else if (context.state.user) {
+      return
     }
+    window.location.assign(node.dataset.redirect)
+    return true
+  })
+  if (!assertions.includes(true)) {
+    setTimeout(() => postready(context))
   }
 }
 /**
@@ -166,48 +169,47 @@ const guard = (context: IContext) => {
 const conditional = (name: string, condition: boolean) => (
   context: IContext
 ) => {
+  if (!context.state.ready) {
+    return
+  }
   const data = document.querySelectorAll(`[data-authpack="${name}"]`)
   const nodes = [...data] as HTMLElement[]
   if (!nodes.length) {
     return
   }
-  if (context.state.ready) {
-    nodes.forEach(node => {
-      if (!node.dataset.property) {
-        const message = `Authpack ${name} tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+  nodes.forEach(node => {
+    if (!node.dataset.property) {
+      const message = `Authpack ${name} tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+      console.warn(message)
+      return
+    }
+    if (!node.dataset.validate) {
+      const message = `Authpack ${name} tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+      console.warn(message)
+      return
+    }
+    let valid = false
+    node.dataset.display = node.dataset.display || node.style.display
+    const steps = node.dataset.property.split('.').map(i => i.trim())
+    const value = steps.reduce(
+      (accum, next) => accum && accum[next],
+      context.state
+    )
+    switch (node.dataset.validate) {
+      case 'present':
+        if (!!value) valid = true
+        break
+      case 'missing':
+        if (!value) valid = true
+        break
+      default:
+        const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
         console.warn(message)
         return
-      }
-      if (!node.dataset.validate) {
-        const message = `Authpack ${name} tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
-        console.warn(message)
-        return
-      }
-      let valid = false
-      node.dataset.display = node.dataset.display || node.style.display
-      const steps = node.dataset.property.split('.').map(i => i.trim())
-      const value = steps.reduce(
-        (accum, next) => accum && accum[next],
-        context.state
-      )
-      switch (node.dataset.validate) {
-        case 'present':
-          if (!!value) valid = true
-          break
-        case 'missing':
-          if (!value) valid = true
-          break
-        default:
-          const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
-          console.warn(message)
-          return
-      }
-      node.style.display =
-        valid === condition
-          ? node.dataset.display || node.style.display
-          : 'none'
-    })
-  }
+    }
+    node.style.display =
+      valid === condition ? node.dataset.display || node.style.display : 'none'
+  })
 }
 /**
  * Show an element when it satisfies any of the conditions.
