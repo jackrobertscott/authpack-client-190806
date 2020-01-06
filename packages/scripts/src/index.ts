5a58aa6184c851b73bae0572e3aee112b4d11165
Jack Scott <jack.rob.scott@gmail.com>
2020-01-06 13:30:24 +0800

    improved all script tags

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -9,6 +9,23 @@ interface IContext {
   state: IPlugin
 }
 /**
+ * Hide contents of page until ready.
+ */
+const preready = () => {
+  if (document.body.dataset.ready !== 'true') {
+    document.body.style.display = 'none'
+  }
+}
+/**
+ * Show contents of page once is ready.
+ */
+const postready = (context: IContext) => {
+  if (context.state.ready && document.body.dataset.ready !== 'true') {
+    document.body.dataset.ready = 'true'
+    document.body.style.display = 'initial'
+  }
+}
+/**
  * Run initial events once the page has loaded.
  */
 setTimeout(() => preready())
@@ -46,55 +63,23 @@ const load = () => {
   return new Authpack(config)
 }
 /**
- * Hide contents of page until ready.
- */
-const preready = () => {
-  if (document.body.dataset.ready !== 'true') {
-    document.body.style.display = 'none'
-  }
-}
-/**
- * Show contents of page once is ready.
- */
-const postready = (context: IContext) => {
-  if (context.state.ready && document.body.dataset.ready !== 'true') {
-    document.body.dataset.ready = 'true'
-    document.body.style.display = 'initial'
-  }
-}
-/**
- * Redirect unauthenticated users from protected pages.
- */
-const guard = (context: IContext) => {
-  const node: HTMLElement = document.querySelector(`[data-authpack="guard"]`)
-  if (!node) {
-    setTimeout(() => postready(context))
-    return
-  }
-  if (!node.dataset.redirect) {
-    const message = `Authpack guard tag missing "data-redirect" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n></div>`
-    console.warn(message)
-    return
-  }
-  if (context.state.ready && !context.state.user) {
-    window.location.assign(node.dataset.redirect)
-    return
-  }
-  setTimeout(() => postready(context))
-}
-/**
  * Open the gadgets when item is clicked.
  */
 const plugin = (context: IContext) => {
-  const node: HTMLElement = document.querySelector(`[data-authpack="plugin"]`)
-  if (!node) {
+  const data = document.querySelectorAll(`[data-authpack="plugin"]`)
+  const nodes = [...data] as HTMLElement[]
+  if (!nodes.length) {
     return
   }
-  if (node.dataset.listening !== 'true') {
-    node.dataset.listening = 'true'
-    node.addEventListener('click', event => {
-      context.authpack.plugin.show()
-      event.preventDefault()
+  if (context.state.ready) {
+    nodes.forEach(node => {
+      if (node.dataset.listening !== 'true') {
+        node.dataset.listening = 'true'
+        node.addEventListener('click', event => {
+          context.authpack.plugin.show()
+          event.preventDefault()
+        })
+      }
     })
   }
 }
@@ -107,72 +92,128 @@ const replace = (context: IContext) => {
   if (!nodes.length) {
     return
   }
-  if (context.state.ready && context.state.user) {
+  if (context.state.ready) {
     nodes.forEach(node => {
-      if (!node.dataset.value) {
-        const message = `Authpack replace tag missing "data-value" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-value="user.name"\n></div>`
+      if (!node.dataset.property) {
+        const message = `Authpack replace tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="replace"\n\tdata-property="user.name"\n></div>`
         console.warn(message)
         return
       }
-      const steps = node.dataset.value.split('.').map(i => i.trim())
-      const value = steps.reduce((accum, next) => accum[next], context.state)
+      const steps = node.dataset.property.split('.').map(i => i.trim())
+      const value = steps.reduce(
+        (accum, next) => accum && accum[next],
+        context.state
+      )
       node.textContent =
         typeof value === 'object' ? JSON.stringify(value, null, 2) : value
     })
   }
 }
 /**
- * Show an element when it satisfies any of the conditions.
+ * Redirect unauthenticated users from protected pages.
  */
-const show = (context: IContext) => {
-  const data = document.querySelectorAll(`[data-authpack="show"]`)
+const guard = (context: IContext) => {
+  const data = document.querySelectorAll(`[data-authpack="guard"]`)
+  const nodes = [...data] as HTMLElement[]
+  if (!nodes.length) {
+    setTimeout(() => postready(context))
+    return
+  }
+  if (context.state.ready) {
+    const assertions = nodes.map(node => {
+      if (!node.dataset.redirect) {
+        const message = `Authpack guard tag missing "data-redirect" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n></div>`
+        console.warn(message)
+        return
+      }
+      if (node.dataset.property) {
+        if (!node.dataset.validate) {
+          const message = `Authpack guard tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="guard"\n\tdata-redirect="/home"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+          console.warn(message)
+          return
+        }
+        const steps = node.dataset.property.split('.').map(i => i.trim())
+        const value = steps.reduce(
+          (accum, next) => accum && accum[next],
+          context.state
+        )
+        switch (node.dataset.validate) {
+          case 'present':
+            if (!!value) return
+            break
+          case 'missing':
+            if (!value) return
+            break
+          default:
+            const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
+            console.warn(message)
+            return
+        }
+      } else if (context.state.user) {
+        return
+      }
+      window.location.assign(node.dataset.redirect)
+      return true
+    })
+    if (!assertions.includes(true)) {
+      setTimeout(() => postready(context))
+    }
+  }
+}
+/**
+ * Helper method for show and hide nodes.
+ */
+const conditional = (name: string, condition: boolean) => (
+  context: IContext
+) => {
+  const data = document.querySelectorAll(`[data-authpack="${name}"]`)
   const nodes = [...data] as HTMLElement[]
   if (!nodes.length) {
     return
   }
   if (context.state.ready) {
     nodes.forEach(node => {
-      const assertions = [
-        !node.dataset.user ||
-          Boolean(node.dataset.user === 'true' && !!context.state.user),
-        !node.dataset.team ||
-          Boolean(node.dataset.team === 'true' && !!context.state.team),
-        !node.dataset.plan ||
-          node.dataset.plan
-            .split(',')
-            .map(plan => plan.trim())
-            .includes(context.state.plan.tag),
-      ]
+      if (!node.dataset.property) {
+        const message = `Authpack ${name} tag missing "data-property" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+        console.warn(message)
+        return
+      }
+      if (!node.dataset.validate) {
+        const message = `Authpack ${name} tag missing "data-validate" attribute i.e.:\n\n<div\n\tdata-authpack="${name}"\n\tdata-property="user"\n\tdata-validate="present"\n></div>`
+        console.warn(message)
+        return
+      }
+      let valid = false
       node.dataset.display = node.dataset.display || node.style.display
-      node.style.display = assertions.includes(false)
-        ? 'none'
-        : node.dataset.display || node.style.display
+      const steps = node.dataset.property.split('.').map(i => i.trim())
+      const value = steps.reduce(
+        (accum, next) => accum && accum[next],
+        context.state
+      )
+      switch (node.dataset.validate) {
+        case 'present':
+          if (!!value) valid = true
+          break
+        case 'missing':
+          if (!value) valid = true
+          break
+        default:
+          const message = `Authpack "data-validate" attribute is incorrect: ${node.dataset.validate}`
+          console.warn(message)
+          return
+      }
+      node.style.display =
+        valid === condition
+          ? node.dataset.display || node.style.display
+          : 'none'
     })
   }
 }
 /**
+ * Show an element when it satisfies any of the conditions.
+ */
+const show = conditional('show', true)
+/**
  * Hide an element when it satisfies any of the conditions.
  */
-const hide = (context: IContext) => {
-  const data = document.querySelectorAll(`[data-authpack="hide"]`)
-  const nodes = [...data] as HTMLElement[]
-  if (!nodes.length) {
-    return
-  }
-  if (context.state.ready && context.state.user) {
-    nodes.forEach(node => {
-      const assertions = [
-        !node.dataset.plan ||
-          node.dataset.plan
-            .split(',')
-            .map(plan => plan.trim())
-            .includes(context.state.plan.tag),
-        // todo: more conditions
-      ]
-      node.dataset.display = node.dataset.display || node.style.display
-      node.style.display = assertions.includes(true)
-        ? 'none'
-        : node.dataset.display || node.style.display
-    })
-  }
-}
+const hide = conditional('hide', false)
